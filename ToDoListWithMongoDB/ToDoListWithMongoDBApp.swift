5a189fc9708d5b023f9497ad7fd7ad1e4d5efef4
Roman Riepa <pwroma@gmail.com>
2022-09-26 12:29:51 +0300

    Initial Commit

---
+++ ToDoListWithMongoDB/ToDoListWithMongoDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListWithMongoDBApp.swift
+//  ToDoListWithMongoDB
+//
+//  Created by Roman Riepa on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListWithMongoDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
